ac7ad53d3d9db16275736f6224f8e154c25e75d1
Student <jsitbon@qcc.cuny.edu>
2019-07-25 18:07:18 -0400

    update

--- client/src/employee/employee.js
+++ client/src/employee/employee.js
@@ -5,24 +5,25 @@ class Employee extends Component {
     constructor() {
         super();
         this.state = {
-            employees: []
+            employees: [],
+            Name: "",
+            EmpCode:0,
+            Salary: 0,
         }
     }
 
     async componentDidMount() {
         try {
-            const res = await axios.get("/employees");
-            console.log(res.data)
-            this.setState({
-                employees: res.data
-            })
+           this.refresh();
         } catch (error) {
             console.error(error);
         }
     }
 
     handleChange = (event) =>{
-        
+      this.setState({
+          [event.target.name]: event.target.value
+      })
     }
 
     deleteEmployee = async (id) => {
@@ -30,6 +31,21 @@ class Employee extends Component {
             await axios.delete(`/employees/${id}`);
             console.log(`Id: ${id} was deleted`);
            this.refresh();
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    addEmployee = async (event)=> {
+        event.preventDefault();
+        const {Name, EmpCode, Salary,} = this.state
+        try{
+           await axios.post("/employees", {Name, EmpCode, Salary})
+           alert("Employee Successfully added")
+           const res = await axios.get("/employees")
+           this.setState({
+               employees: res.data
+           })
         }catch(error){
             console.log(error)
         }
@@ -59,6 +75,7 @@ class Employee extends Component {
                         <input name="Name" placeholder="Please Enter Employee Name Here!" onChange ={this.handleChange}/>
                         <input name="EmpCode" placeholder="Place the Employee Code here It's a number" onChange ={this.handleChange}/>
                         <input name ="Salary" placeholder="Please enter the Employees Salary - It's a number as well" onChange ={this.handleChange}/>
+                        <button type="submit">Submit</button>
                     </form>
                 </div>
             )
